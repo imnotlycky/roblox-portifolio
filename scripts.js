f78770f0c3ea47e95a1bf43077fbfab86367a066
imnotlycky <sapsu.liekola@gmail.com>
2024-10-03 16:29:24 +0300

    Update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let data = "JobAppId_V1.04"
+
+let jobid = localStorage.getItem(data) || 1
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('nav ul li a').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
@@ -8,3 +12,60 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+document.getElementById("formdc").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let dcName = document.getElementById("dcName").value;
+  let desc = document.getElementById("reason").value;
+  let budget = document.getElementById("budget").value;
+  let deadline = document.getElementById("deadline").value || "No Deadline";
+  let extra = document.getElementById("extraInfo").value || "No Extra Info";
+
+  const request = new XMLHttpRequest();
+  request.open("POST", "https://discord.com/api/webhooks/1291389655429812366/DDj8ZvVjCCk7Y89gpNtdwALfwrP_XqYMo8YpzefZolSQlzqTuH8Z7NobPP68yBgcODUO")
+
+  request.setRequestHeader("Content-type", "application/json")
+
+  const date = new Date();
+
+  let timestamp = date.toISOString();
+
+  let params = {
+      "name": "JobAppGuy",
+      "content": null,
+      "embeds": [
+        {
+          "title": "Job App",
+          "description": `Why (we | i) want to hire you:\n${desc}\n\nMy | our Budget is:\n${budget}\n\nDeadline is on:\n${deadline}\n\nExtra info about the Project:\n${extra}`,
+          "color": 255,
+          "fields": [
+            {
+              "name": "Discord Username",
+              "value": `${dcName}`
+            },
+            {
+              "name": "Application Id",
+              "value": `${jobid}`
+            }
+          ],
+          "footer": {
+            "text": "Submit Time"
+          },
+          "timestamp": timestamp
+        }
+      ],
+      "username": "JobAppGuy",
+      "attachments": []
+  }
+
+  request.send(JSON.stringify(params));
+
+  request.onload = () => {
+    if (request.status == 204) {
+      jobid++;
+      localStorage.setItem(data, jobid)
+    }
+  }
+})
